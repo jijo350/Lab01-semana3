526bb57dabef065c08600f446ff9a5ecfc52199d
Freddy Angelo <freddycordova3002@gmail.com>
2025-04-09 10:28:12 -0500

    Cuarto commit

--- Ejemplo/Program.cs
+++ Ejemplo/Program.cs
@@ -13,6 +13,7 @@ namespace Ejemplo
             Console.WriteLine("Bienvenidos al curso C#");
             Console.WriteLine("Bienvenidos al curso C#);
             Console.Write("Bienvenidos al curso C#");
+            Console.WriteLine("Bienvenidos al curso C#");
             Console.ReadKey();
         }
     }
